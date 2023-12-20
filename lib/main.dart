23ded8ae607e293c7b682da008505f7422d90d9c
david <david.emanoel.706@ufrn.edu.br>
2023-12-20 16:20:58 -0300

    build home

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:textedit/screens/login/login_view.dart';
+import 'package:textedit/utils/themes.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(MainApp());
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [MyColors.pBlue, MyColors.pBlueLight],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter
+            )
+          ),
+          child: const LoginView(),
         ),
-      ),
+      )
     );
   }
 }
