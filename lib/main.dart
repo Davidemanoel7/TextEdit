02007397a0a8457049cee58975c6f04d044f1a28
david <david.emanoel.706@ufrn.edu.br>
2023-12-26 20:49:31 -0300

    remove const

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MainApp extends StatelessWidget {
               end: Alignment.bottomCenter
             )
           ),
-          child: const LoginView(),
+          child: LoginView(),
         ),
       )
     );
